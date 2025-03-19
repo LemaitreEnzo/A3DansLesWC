33c8e4b6a2f0b79b894342587a17acfa3512c056
Lemaitre Enzo <lemaitreenzo05@gmail.com>
2025-03-19 14:30:21 +0100

    Update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,2 @@
-const userConsents = true; 
-if (userConsents) {
-    const script = document.createElement('script');
-    script.src = "https://example.com/third-party.js";
-    document.body.appendChild(script);
-}
-
 document.addEventListener('scroll', function(e) {
 }, { passive: true });
-
-setTimeout(() => {
-    console.log("Optimisation en cours...");
-}, 0);
